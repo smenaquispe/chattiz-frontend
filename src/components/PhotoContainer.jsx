79e222b62cc4033d0c2a69820d5f4258b0c46271
smena <sergiosantosmenaq@gmail.com>
2024-03-19 22:42:56 -0500

    logout layout

--- src/components/PhotoContainer.jsx
+++ src/components/PhotoContainer.jsx
@@ -5,7 +5,7 @@ export function PhotoContainer({ pathPhoto }) {
     const [hover, setHover] = useState(false)
     
     return (
-      <div className="relative w-[200px] h-[200px]"
+      <div className="relative w-[200px] h-[200px] rounded-full"
         onMouseEnter={() => setHover(true)}
         onMouseLeave={() => setHover(false)}  
       >
@@ -19,7 +19,7 @@ export function PhotoContainer({ pathPhoto }) {
             hover &&
             <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 rounded-full">
             <div className="text-white text-center">
-                <p>Cambiar</p>
+                <p>Change Photo</p>
             </div>
             </div>
         }
--- src/layouts/Logout.jsx
+++ src/layouts/Logout.jsx
@@ -1,8 +1,8 @@
 export function Logout() {
     return (
         <div className="w-4/5 ml-4">
-            <h2>Logout</h2>
-            <p>Are you sure you want to logout?</p>
+            <h2 className="text-2xl font-bold mb-4 text-purple-600" >Logout</h2>
+            <p className="mb-4">Are you sure you want to logout?</p>
             <button className="bg-red-500 text-white px-4 py-2 rounded-md">Yes</button>
             <button className="bg-gray-200 text-gray-700 px-4 py-2 rounded-md ml-4">No</button>
         </div>
