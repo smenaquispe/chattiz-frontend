28a40892817fb0023aa8b7a84f333ce651e7cc1b
smena <sergiosantosmenaq@gmail.com>
2024-03-19 21:04:13 -0500

    set views

--- src/components/ButtonWithSvg.jsx
+++ src/components/ButtonWithSvg.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-export function ButtonWithSvg({svgName}) {
+export function ButtonWithSvg({svgName, setView}) {
 
     const [svg, setSvg] = useState(svgName);
 
@@ -8,6 +8,7 @@ export function ButtonWithSvg({svgName}) {
         <button className="h-1/3 flex flex-col justify-center w-full items-center hover:bg-purple-500 transition duration-150 ease-in-out rounded-md"
             onMouseEnter={() => setSvg(`${svgName}-hover`)}
             onMouseLeave={() => setSvg(`${svgName}`)}
+            onClick={() => setView(svgName)}
         >
             <img src={`${svg}.svg`} alt="settings" className="w-8 h-8" />
         </button>
--- src/layouts/Modal.jsx
+++ src/layouts/Modal.jsx
@@ -1,21 +1,43 @@
+import { useState } from "react";
 import { ButtonWithSvg } from "../components/ButtonWithSvg";
+import { SETTINGS, PROFILE, LOGOUT } from "../constants/consts";
 
 export function Modal() {
+
+    const [view, setView] = useState(SETTINGS);
+
     return (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
             <div className="bg-white p-8 rounded-lg shadow-lg flex w-[800px] h-[350px]">
-                <div className="w-1/5 border-r-2 border-gray-200">
+                <div className="w-1/5 pr-4 border-r-2 border-gray-200">
                     <ul className="h-full flex flex-col items-center justify-center">
-                        <ButtonWithSvg svgName={"settings"} />
-                        <ButtonWithSvg svgName={"profile"} />
-                        <ButtonWithSvg svgName={"logout"} />
+                        <ButtonWithSvg svgName={SETTINGS} setView={setView} />
+                        <ButtonWithSvg svgName={PROFILE} setView={setView}  />
+                        <ButtonWithSvg svgName={LOGOUT}  setView={setView}  />
                     </ul>
                 </div>
-                <div className="w-4/5 ml-4">
-                    <h2>Profile</h2>
-                    <p>Username: <span>John Doe</span></p>
-                    <p>Email: <span> johndoegmail.com</span></p>
-                </div>
+                {
+                    view === SETTINGS && <div className="w-4/5 ml-4">
+                        <h2>Settings</h2>
+                        <p>Dark mode: <span>On</span></p>
+                        <p>Notifications: <span>On</span></p>
+                    </div>
+                }
+                {
+                    view === PROFILE && <div className="w-4/5 ml-4">
+                        <h2>Profile</h2>
+                        <p>Username: <span>John Doe</span></p>
+                        <p>Email: <span> johndoegmail.com</span></p>
+                    </div>
+                }
+                {
+                    view === LOGOUT && <div className="w-4/5 ml-4">
+                        <h2>Logout</h2>
+                        <p>Are you sure you want to logout?</p>
+                        <button className="bg-red-500 text-white px-4 py-2 rounded-md">Yes</button>
+                        <button className="bg-gray-200 text-gray-700 px-4 py-2 rounded-md ml-4">No</button>
+                    </div>
+                }
             </div>
         </div>
     );
